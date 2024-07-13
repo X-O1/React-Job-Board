ec3b2c5a1d805a4072b7adf8464423af88ffacca
X-O1 <series-x-01@protonmail.com>
2024-07-13 00:01:11 -0700

    Final commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,10 +13,7 @@ const Navbar = () => {
         <div className="flex h-20 items-center justify-between">
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
             {/* <!-- Logo --> */}
-            <NavLink
-              className="flex flex-shrink-0 items-center mr-4"
-              to="/index.html"
-            >
+            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
               <img className="h-10 w-auto" src={logo} alt="React Jobs" />
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
                 React Jobs
